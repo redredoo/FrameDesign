feb199e27f863e1dd829c6bfce232d0f964e3fc5
redredoo <1309625134@qq.com>
2024-07-04 17:17:07 +0800

    test

---
+++ Assets/FrameDesign/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FrameDesign.Example
+{
+    public class Enemy : MonoBehaviour
+    {
+        public GameObject GameOverObj;
+
+        static int mKilledEnemyCount = 0;
+
+        private void OnMouseDown()
+        {
+            Destroy(gameObject);
+            mKilledEnemyCount++;
+            if (mKilledEnemyCount == 10)
+            {
+                GameOverObj.SetActive(true);
+            }
+        }
+    }
+}
+
